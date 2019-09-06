a11d71b0e9ed5d4a7f2012f00b37413a3a1c051b
Artem Rebryk <artemrebrik8099@gmail.com>
2019-09-06 23:33:30 +0300

    change layout for real components

--- src/app/presentation/components/components.ts
+++ src/app/presentation/components/components.ts
@@ -2,9 +2,11 @@
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 export const components = [
     HeaderComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    MainComponent
 ];
