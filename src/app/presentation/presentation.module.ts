9dfecf637a842baf66b22af538bd74e855f70612
Artem Rebryk <artemrebrik8099@gmail.com>
2019-09-06 21:23:02 +0300

    create components module

--- src/app/presentation/presentation.module.ts
+++ src/app/presentation/presentation.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PresentationComponent } from './presentation.component';
-import { RoutingModule } from './router/routing.module';
-import { HeaderComponent } from './components/header/header.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { RoutingModule } from './pages/routing.module';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
-    PresentationComponent,
-    HeaderComponent,
-    SidebarComponent,
-    FooterComponent
+    PresentationComponent
   ],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    ComponentsModule
   ],
   exports: [ PresentationComponent ]
 })
