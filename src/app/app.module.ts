6bfdebf660a1d9fe0ae57fc5d1eab1780566e3cf
Artem Rebryk <artemrebrik8099@gmail.com>
2019-09-06 21:12:16 +0300

    add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PresentationModule } from './presentation/presentation.module';
@@ -12,7 +11,6 @@ import { PresentationModule } from './presentation/presentation.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     PresentationModule
   ],
--- src/app/presentation/presentation.module.ts
+++ src/app/presentation/presentation.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PresentationComponent } from './presentation.component';
+import { RoutingModule } from './routing.module';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [PresentationComponent],
+  declarations: [PresentationComponent, HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RoutingModule
   ],
   exports: [ PresentationComponent ]
 })
