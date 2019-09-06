457ea278fa6451a3f4d3b5b5a84f4616d6677ef0
Artem Rebryk <artemrebrik8099@gmail.com>
2019-09-06 20:36:22 +0300

    create presentation module

--- src/app/presentation/presentation.module.ts
+++ src/app/presentation/presentation.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { PresentationComponent } from './presentation.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PresentationComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [ PresentationComponent ]
 })
 export class PresentationModule { }
