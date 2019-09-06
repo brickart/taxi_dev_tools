1ccd6341876c6a122a928c46caa2747ca5eb68f7
Artem Rebryk <artemrebrik8099@gmail.com>
2019-09-06 21:12:35 +0300

    add footer

--- src/app/presentation/presentation.module.ts
+++ src/app/presentation/presentation.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PresentationComponent } from './presentation.component';
-import { RoutingModule } from './routing.module';
+import { RoutingModule } from './router/routing.module';
 import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [PresentationComponent, HeaderComponent],
+  declarations: [
+    PresentationComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
     RoutingModule
