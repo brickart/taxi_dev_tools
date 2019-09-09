f5ea2c10f316c3f1709d6b09a687e38f4167a859
Artem Rebryk <artemrebrik8099@gmail.com>
2019-09-09 20:15:55 +0300

    create routes

--- src/app/presentation/pages/routing.module.ts
+++ src/app/presentation/pages/routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'emulators', loadChildren: './emulator/emulator.module#EmulatorModule' },
+    { path: 'settings', loadChildren: './settings/settings.module#SettingsModule' },
+    { path: '', redirectTo: '/emulators', pathMatch: 'full'},
+    { path: '**', redirectTo: '' },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
