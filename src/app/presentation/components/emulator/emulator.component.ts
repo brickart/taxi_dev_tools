47e62c6c5b2e6cb125fbdc0b122e36ecf016bbb1
Artem Rebryk <artemrebrik8099@gmail.com>
2019-09-10 19:18:38 +0300

    create component

---
+++ src/app/presentation/components/emulator/emulator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emulator',
+  templateUrl: './emulator.component.html',
+  styleUrls: ['./emulator.component.scss']
+})
+export class EmulatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
